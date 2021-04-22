9e46885e889131d623ffe904965f1d4d84208dff
jesseljones <78154747+jesseljones@users.noreply.github.com>
2021-04-22 08:33:13 -0500

    Add files via upload

---
+++ WPF_JSON_RickandMorty/WPF_JSON_RickandMorty/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_JSON_RickandMorty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string url = "https://rickandmortyapi.com/api/character";
+
+
+            while (url != null)
+
+            {
+
+                using (var client = new HttpClient())
+                {
+                    string jsonData = client.GetStringAsync(url).Result;
+
+                    RickandMortyAPI api = JsonConvert.DeserializeObject<RickandMortyAPI>(jsonData);
+
+                    foreach (var character in api.results)
+                    {
+                        lstCharacter.Items.Add(character);
+                    }
+
+                    url = api.info.next;
+                }
+            }
+        }
+
+        private void lstCharacter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedCharacter = (Character)lstCharacter.SelectedItem;
+            NewWindow imageWindow = new NewWindow();
+            imageWindow.SelectedCharacter = selectedCharacter;
+            imageWindow.GetImage(selectedCharacter);
+            imageWindow.Show();
+
+            
+        
+        }
+    }
+}
